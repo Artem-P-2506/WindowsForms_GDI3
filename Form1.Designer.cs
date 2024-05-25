799d6dc61c08ad1a8ddcf0f70fcc6442bec6faf9
Artem Pushko <artempushko04@gmail.com>
2024-05-25 22:53:45 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace WindowsForms_GDI3
+{
+    partial class Game
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxKnight = new System.Windows.Forms.PictureBox();
+            this.pictureBoxSkeleton = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxKnight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSkeleton)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxKnight
+            // 
+            this.pictureBoxKnight.Location = new System.Drawing.Point(36, 334);
+            this.pictureBoxKnight.Name = "pictureBoxKnight";
+            this.pictureBoxKnight.Size = new System.Drawing.Size(168, 226);
+            this.pictureBoxKnight.TabIndex = 0;
+            this.pictureBoxKnight.TabStop = false;
+            this.pictureBoxKnight.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // pictureBoxSkeleton
+            // 
+            this.pictureBoxSkeleton.Location = new System.Drawing.Point(856, 353);
+            this.pictureBoxSkeleton.Name = "pictureBoxSkeleton";
+            this.pictureBoxSkeleton.Size = new System.Drawing.Size(194, 207);
+            this.pictureBoxSkeleton.TabIndex = 1;
+            this.pictureBoxSkeleton.TabStop = false;
+            this.pictureBoxSkeleton.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBoxSkeleton_Paint);
+            // 
+            // Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1094, 596);
+            this.Controls.Add(this.pictureBoxSkeleton);
+            this.Controls.Add(this.pictureBoxKnight);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Name = "Game";
+            this.Text = "Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Game_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxKnight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSkeleton)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxKnight;
+        private System.Windows.Forms.PictureBox pictureBoxSkeleton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsForms_GDI3
+{
+    public partial class Game : Form
+    {
+        private bool isJumping = false;
+        private int jumpHeight = 120; // Висота стрибка
+        private int jumpSpeed = 40;   // Швидкість стрибка
+        private int gravity = 30;     // Гравітація
+
+        public Game()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Bitmap background = new Bitmap("E:\\Programs\\Microsoft Visual Studio\\Проекты\\WindowsForms_GDI3\\Images\\background.png");
+            e.Graphics.DrawImage(background, new Rectangle(0, 0, 1100, 600));
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Bitmap knight = new Bitmap("E:\\Programs\\Microsoft Visual Studio\\Проекты\\WindowsForms_GDI3\\Images\\Knight sprites.png");
+            e.Graphics.DrawImage(knight, new Rectangle(0, 0, 2500, 1400));
+        }
+
+        private void pictureBoxSkeleton_Paint(object sender, PaintEventArgs e)
+        {
+            Bitmap knight = new Bitmap("E:\\Programs\\Microsoft Visual Studio\\Проекты\\WindowsForms_GDI3\\Images\\Skeleton sprites.png");
+            e.Graphics.DrawImage(knight, new Rectangle(-2300, 0, 2500, 1400));
+        }
+
+
+
+        private void Game_KeyDown(object sender, KeyEventArgs e)
+        {
+            int step = 10;
+            if (e.KeyCode == Keys.Right)
+            {
+                pictureBoxKnight.Invoke(new Action(() => pictureBoxKnight.Left += step));
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                pictureBoxKnight.Invoke(new Action(() => pictureBoxKnight.Left -= step));
+            }
+            if (e.KeyCode == Keys.Up && !isJumping)
+            {
+                isJumping = true;
+                new Thread(Jump).Start();
+            }
+        }
+
+        private void Jump()
+        {
+            int originalY = pictureBoxKnight.Bottom;
+
+            while (pictureBoxKnight.Bottom > originalY - jumpHeight)
+            {
+                pictureBoxKnight.Invoke(new Action(() => pictureBoxKnight.Top -= jumpSpeed));
+                Thread.Sleep(50);
+            }
+
+            while (pictureBoxKnight.Bottom < originalY)
+            {
+                pictureBoxKnight.Invoke(new Action(() => pictureBoxKnight.Top += gravity));
+                Thread.Sleep(50);
+            }
+
+            isJumping = false;
+        }
+    }
+}
